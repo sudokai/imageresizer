9f1c8c867314ea35079c7dd7f472f350abab7500
Kaixi Luo <1402931+kailt@users.noreply.github.com>
2018-10-24 00:39:34 +0200

    feat: implement fit resize operation
    fit resizes the image without cropping the edges,
    by making the image smaller than the target width/height
    as needed.

--- imager/vips.h
+++ imager/vips.h
@@ -37,3 +37,16 @@ int vips_thumbnail_cgo(void *buf, size_t len, VipsImage **out, int width, int he
         "intent", VIPS_INTENT_PERCEPTUAL,
         NULL);
 }
+
+int vips_image_new_cgo(int imageType, void *buf, size_t len, VipsImage **out) {
+    int err = 1;
+    switch (imageType) {
+    case JPEG:
+        err = vips_jpegload_buffer(buf, len, out, "access", VIPS_ACCESS_SEQUENTIAL, NULL);
+        break;
+    case PNG:
+         err = vips_pngload_buffer(buf, len, out, "access", VIPS_ACCESS_SEQUENTIAL, NULL);
+         break;
+    }
+    return err;
+}
