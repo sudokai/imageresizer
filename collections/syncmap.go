616a341793eb390d28ef14c20c009b7b4da472d3
Kaixi Luo <1402931+kailt@users.noreply.github.com>
2018-10-28 21:51:22 +0100

    fix: improve randomness of SyncMap's GetRand()
    Use a slice to index elements so we can get a true random index,
    instead of just fetching the first value of the map,
    which relies on go's undefined ordering of map elements.

--- collections/syncmap.go
+++ collections/syncmap.go
@@ -1,15 +1,30 @@
 package collections
 
-import "sync"
+import (
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type item struct {
+	idx int
+	key string
+	val interface{}
+}
 
 type SyncMap struct {
-	data map[string]interface{}
+	indexed []*item
+	keyed   map[string]*item
 	sync.RWMutex
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
 }
 
 func NewSyncMap() *SyncMap {
 	sm := &SyncMap{
-		data: map[string]interface{}{},
+		keyed: map[string]*item{},
 	}
 	return sm
 }
@@ -17,39 +32,64 @@ func NewSyncMap() *SyncMap {
 func (sm *SyncMap) Get(key string) interface{} {
 	sm.RLock()
 	defer sm.RUnlock()
-	return sm.data[key]
+	elem, ok := sm.keyed[key]
+	if !ok {
+		return nil
+	}
+	return elem.val
 }
 
 func (sm *SyncMap) Put(key string, val interface{}) {
 	sm.Lock()
 	defer sm.Unlock()
-	sm.data[key] = val
+	if elem, ok := sm.keyed[key]; ok {
+		elem.val = val
+		return
+	}
+	elem := &item{idx: len(sm.indexed), key: key, val: val}
+	sm.keyed[key] = elem
+	sm.indexed = append(sm.indexed, elem)
 }
 
 func (sm *SyncMap) Remove(key string) {
 	sm.Lock()
 	defer sm.Unlock()
-	delete(sm.data, key)
+	elem, ok := sm.keyed[key]
+	if !ok {
+		return
+	}
+
+	delete(sm.keyed, key)
+
+	lastIdx := len(sm.indexed) - 1
+	if elem.idx < lastIdx {
+		// elem.idx is not the last
+		// swap the element we delete with the last element
+		last := sm.indexed[lastIdx]
+		last.idx = elem.idx
+		sm.indexed[elem.idx] = last
+	}
+	// delete the last element
+	sm.indexed[lastIdx] = nil // https://github.com/golang/go/wiki/SliceTricks
+	sm.indexed = sm.indexed[:lastIdx]
 }
 
 func (sm *SyncMap) Size() int {
 	sm.RLock()
 	defer sm.RUnlock()
-	return len(sm.data)
+	return len(sm.indexed)
 }
 
 func (sm *SyncMap) HasKey(key string) bool {
 	sm.RLock()
 	defer sm.RUnlock()
-	_, ok := sm.data[key]
+	_, ok := sm.keyed[key]
 	return ok
 }
 
 func (sm *SyncMap) GetRand() interface{} {
 	sm.RLock()
 	defer sm.RUnlock()
-	for _, v := range sm.data {
-		return v
-	}
-	return nil
+	elem := sm.indexed[rand.Intn(len(sm.indexed))]
+	return sm.Get(elem.key)
 }
---
+++ collections/syncmap_test.go
@@ -0,0 +1,72 @@
+package collections
+
+import (
+	"math"
+	"testing"
+)
+
+func TestSyncMap_Get(t *testing.T) {
+	sm := NewSyncMap()
+	sm.Put("a", 1)
+	sm.Put("b", 2)
+	sm.Put("a", 3)
+
+	if sm.Get("a") != 3 || sm.Get("b") != 2 {
+		t.Errorf("Map returns wrong value")
+	}
+
+	if sm.Get("c") != nil {
+		t.Errorf("Map should return nil element for non-existing key")
+	}
+}
+
+func TestSyncMap_Put(t *testing.T) {
+	sm := NewSyncMap()
+	sm.Put("a", 1)
+	sm.Put("b", 2)
+	sm.Put("a", 3)
+
+	if len(sm.indexed) != 2 {
+		t.Errorf("Map's indexed slice doesn't have the correct length: %d", len(sm.indexed))
+	}
+	if sm.keyed["a"].idx != 0 || sm.keyed["b"].idx != 1 {
+		t.Errorf("Map elements' indexes are out of sync")
+	}
+}
+
+func TestSyncMap_Remove(t *testing.T) {
+	sm := NewSyncMap()
+	sm.Put("a", 1)
+	sm.Put("b", 2)
+	sm.Put("a", 3)
+	sm.Remove("c")
+
+	if len(sm.indexed) != 2 || len(sm.keyed) != 2 {
+		t.Errorf("Removing non existing key actually removed an element")
+	}
+
+	sm.Remove("a")
+
+	if len(sm.indexed) != 1 || len(sm.keyed) != 1 {
+		t.Errorf("Element wasn't removed")
+	}
+}
+
+func TestSyncMap_GetRand(t *testing.T) {
+	sm := NewSyncMap()
+	sm.Put("a", 0)
+	sm.Put("b", 1)
+	sm.Put("c", 2)
+	sm.Put("d", 3)
+	counts := []int{0, 0, 0, 0}
+
+	for i := 0; i < 100000; i++ {
+		counts[sm.GetRand().(int)]++
+	}
+
+	for _, x := range counts {
+		if math.Abs(float64(x-25000)) > 1000 {
+			t.Errorf("Not random enough")
+		}
+	}
+}
