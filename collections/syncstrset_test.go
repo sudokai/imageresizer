843fa6235b025514c665037b47896d76351ee175
Kaixi Luo <1402931+kailt@users.noreply.github.com>
2018-10-28 23:20:55 +0100

    fix: add test of SyncStrSet's Slice()

--- collections/syncstrset_test.go
+++ collections/syncstrset_test.go
@@ -2,7 +2,7 @@ package collections
 
 import "testing"
 
-func TestSetIntersect(t *testing.T) {
+func TestSyncStrSet_Intersect(t *testing.T) {
 	s1 := NewSyncStrSet()
 	s1.Add("a", "b", "c", "d")
 	s2 := NewSyncStrSet()
@@ -14,5 +14,26 @@ func TestSetIntersect(t *testing.T) {
 		intersection.Contains("e") ||
 		intersection.Contains("f") {
 		t.Errorf("Set contains wrong elements: %v", intersection.Slice())
+	}
+}
+
+func TestSyncStrSet_Slice(t *testing.T) {
+	set := NewSyncStrSet()
+	set.Add("a", "b", "c", "d")
+	slice := set.Slice()
+	for _, x := range []string{"a", "b", "c", "d"} {
+		var found bool
+		for _, v := range slice {
+			found = x == v
+			if found {
+				break
+			}
+		}
+		if !found {
+			t.Errorf("Slice doesn't contain all the set's elements")
+		}
+	}
+	if len(slice) != 4 {
+		t.Errorf("Wrong slice length")
 	}
 }
